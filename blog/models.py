80076c438db12c17f27544df67cbd0299b64fec9
EverDuvan <everduvanhdez@gmail.com>
2024-03-15 10:08:58 -0500

    update

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='blog/images/')
+    date = models.DateField( auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField()
+    read_only_fields = ['created_at', 'updated_at']
+
+    def __str__(self):
+        return self.title
--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -18,12 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import include
+from django.urls import include, path
 from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('blog/', include('blog.urls')),
 ]
 
 if settings.DEBUG:
